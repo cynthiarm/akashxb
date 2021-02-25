8b1db54c661be5154f761cec59d1dc0e5a1d6f9a
cynthiarm21 <cyntiff@gmail.com>
2021-02-25 13:38:42 -0800

    login

--- js/main.js
+++ js/main.js
@@ -625,6 +625,9 @@ $(function() {
     /* ================================================
        Login form
        ================================================ */
+
+
+       
     $('.message a').click(function(){
         $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
      });
