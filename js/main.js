37878aa4a3641fff4f79f7ff82bcec9b2cdea94e
cynthiarm21 <cyntiff@gmail.com>
2021-02-26 11:23:01 -0800

    login

--- js/main.js
+++ js/main.js
@@ -630,15 +630,43 @@ $(function() {
         warning.alert("working");
     }*/
 
+
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          // User is signed in.
+          document.getElementById("user_div").style.display = "block";
+          document.getElementById("login_div").style.display = "none";
+        } else {
+          // No user is signed in.
+          document.getElementById("user_div").style.display = "none";
+          document.getElementById("login_div").style.display = "block";
+        }
+      });
+
+    
     function login() {
         var userEmail = document.getElementById("email_field").value;
         var userPass = document.getElementById("password_field").value;
 
-        window.alert(userEmail +  " "  + userPass);
+
+
+        firebase.auth().signInWithEmailAndPassword(userEmail, userPass)
+        .then((user) => {
+          // Signed in
+          // ...
+        })
+        .catch((error) => {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+        });
+
+        window.alert("Error :  " + errorMessage);
+        });
+    
     }
-
-
-
+    firebase.auth().signOut()
+      
+    }
 
 
   /* $('.message a').click(function(){
